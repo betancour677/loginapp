8af5f09b1e1217262cea70b53572feb34587562e
emmanuel <emmanuel.betancour@gmail.com>
2023-07-30 22:45:23 -0400

    V2

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -7,29 +7,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.loginapp.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextFirstName, editTextLastName, editTextEmail, editTextPassword;
     private Button buttonSubmit;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        editTextFirstName = findViewById(R.id.editTextFirstName);
-        editTextLastName = findViewById(R.id.editTextLastName);
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        buttonSubmit = findViewById(R.id.buttonSubmit);
-
-        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String firstName = editTextFirstName.getText().toString();
-                String lastName = editTextLastName.getText().toString();
-                String email = editTextEmail.getText().toString();
-                String password = editTextPassword.getText().toString();
+                String firstName = binding.editTextFirstName.getText().toString();
+                String lastName = binding.editTextLastName.getText().toString();
+                String email = binding.editTextEmail.getText().toString();
+                String password = binding.editTextPassword.getText().toString();
 
                 if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
                     // Al menos un campo está vacío, mostramos el Toast de aviso
@@ -43,3 +42,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+
